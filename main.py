866b610aa647ae301045df0f10c2a0916c47bd01
Greta Lin Risgin <145865431+fluffycowfluffy@users.noreply.github.com>
2025-05-11 20:36:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#import dash
+from src.interface import run_app
+#from src.recommender import cafe_recommender
+
+# Pull the app from interface.py
+app = run_app()
+
+# Run the app
+if __name__ == "__main__":
+   app = app.run(debug = True)
